c2bf7684ab68fc2ff33efed947b56b55c82b07f2
Brandon Rorie <50763955+btror@users.noreply.github.com>
2020-10-30 22:40:01 -0400

    Update name_schema.js

--- models/name_schema.js
+++ models/name_schema.js
@@ -1,17 +1,13 @@
 const mongoose = require("mongoose")
-
-
 const nameSchema = new mongoose.Schema({
     title: {
         type: String,
         trim: true,
         required: true,
-        maxLength: 25,
     },
     budget: {
         type: Number,
         required: true,
-        
     },
     color: {
         type: String,
